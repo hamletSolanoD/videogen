06ab7711d982ad4cd0ca515ad6ad864f4d2f7c2e
Edgar Hamlet Solano Diaz <hamletsolanod@gmail.com>
2025-01-21 16:49:04 -0700

    Feat: reinit project

--- src/pages/api/trpc/[trpc].ts
+++
@@ -1,8 +0,0 @@
-import { createNextApiHandler } from '@trpc/server/adapters/next';
-import { appRouter } from '../../../server/routers';
-import { createContext } from '../../../server/context';
-
-export default createNextApiHandler({
-  router: appRouter,
-  createContext,
-});
